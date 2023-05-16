89297e3db2faa8abfeb9769df4fd14e9d2cb3566
Andrei Bexa <andreibexa@gmail.com>
2023-05-16 16:17:05 +0300

    Rename H-Bridge pins

--- include/pins_line_follower.h
+++ include/pins_line_follower.h
@@ -1,11 +1,11 @@
-#ifndef PINS_H
-#define PINS_H
+#ifndef PINS_LINE_FOLLOWER_H
+#define PINS_LINE_FOLLOWER_H
 
 // H-Bridge (L9110S) Dual Channel - 2 DC motors
-#define HB_A1 = 3; // PWM
-#define HB_A2 = 2;
+#define HB_A_IA = 3; // PWM
+#define HB_A_IB = 2;
 
-#define HB_B1 = 5; // PWM
-#define HB_B2 = 4;
+#define HB_B_IA = 5; // PWM
+#define HB_B_IB = 4;
 
 #endif
