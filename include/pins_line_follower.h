1541423ee9188fe2294c82b4d2da938e38c0a665
Andrei Bexa <andreibexa@gmail.com>
2023-05-16 15:32:47 +0300

    Add Pins for H-Bridge (L9110S) Dual Channel

---
+++ include/pins_line_follower.h
@@ -0,0 +1,11 @@
+#ifndef PINS_H
+#define PINS_H
+
+// H-Bridge (L9110S) Dual Channel - 2 DC motors
+#define HB_A1 = 3; // PWM
+#define HB_A2 = 2;
+
+#define HB_B1 = 5; // PWM
+#define HB_B2 = 4;
+
+#endif
