462a7dfde62dd460a4862186186ffdc45e7d37ed
Andrei Bexa <andreibexa@gmail.com>
2023-06-12 07:16:10 +0300

    ADD Led matrix module

--- src/jade_unoplus/led_matrix/led_matrix.h
+++ src/jade_unoplus/led_matrix/led_matrix.h
@@ -1,9 +1,11 @@
 #ifndef SRC_JADE_UNOPLUS_LED_MATRIX_LED_MATRIX_H_
 #define SRC_JADE_UNOPLUS_LED_MATRIX_LED_MATRIX_H_
+#include "jade_unoplus/pins_jade_unoplus.h"
 #include <Arduino.h>
+#include <LedControl.h>
 
-void loop_matrix();
+void loopMatrix();
 void printByte(byte character[]);
-void setup_matrix();
+void setupMatrix();
 
-#endif // SRC_JADE_UNOPLUS_LED_MATRIX_LED_MATRIX_H_
+#endif  // SRC_JADE_UNOPLUS_LED_MATRIX_LED_MATRIX_H_
